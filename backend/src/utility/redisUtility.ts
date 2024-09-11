675075e0ded16ee7d0732e2e6ebd4d9934e880bc
Harshit Dubey <harshit@Harshits-Laptop.local>
2024-09-11 18:49:45 +0530

    Added redis url

--- backend/src/utility/redisUtility.ts
+++ backend/src/utility/redisUtility.ts
@@ -1,6 +1,6 @@
 import { createClient } from "redis";
 
-const redisClient = createClient();
+const redisClient = createClient({ url: `${process.env.REDIS_URL}` });
 redisClient
   .connect()
   .catch((err) => console.error("Redis Connection Error:", err));
