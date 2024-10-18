432476037163f798f3f6310a36920a8827f8b2d0
Harshit Dubey <harshit@Harshits-Laptop.local>
2024-10-19 01:16:26 +0530

    redis tls

--- backend/src/utility/redisUtility.ts
+++ backend/src/utility/redisUtility.ts
@@ -3,8 +3,6 @@ import { createClient } from "redis";
 const redisClient = createClient({
   url: process.env.REDIS_URL,
   socket: {
-    tls: `${process.env.REDIS_URL}`.match(/rediss:/) != null,
-    rejectUnauthorized: false,
     reconnectStrategy: (retries) => Math.min(retries * 100, 3000),
   },
 });
