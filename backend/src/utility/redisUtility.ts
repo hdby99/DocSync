b94536fba560b9a2df02b3aecaf5812aa7711649
Harshit Dubey <harshit@Harshits-Laptop.local>
2024-10-19 00:55:29 +0530

    tls

--- backend/src/utility/redisUtility.ts
+++ backend/src/utility/redisUtility.ts
@@ -1,9 +1,11 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
+  url: process.env.REDIS_URL,
   socket: {
     tls: true,
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, // Accept self-signed certificates
+    reconnectStrategy: (retries) => Math.min(retries * 100, 3000),
   },
 });
 
